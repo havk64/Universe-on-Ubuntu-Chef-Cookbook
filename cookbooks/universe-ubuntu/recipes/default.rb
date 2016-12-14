a435a583f8330f0f3432826c80ef4c7728af0f54
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-14 19:37:45 -0200

    Add basic apt packages

--- cookbooks/universe-ubuntu/recipes/default.rb
+++ cookbooks/universe-ubuntu/recipes/default.rb
@@ -5,3 +5,23 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'apt::default'
+
+package 'install basic apt packages' do
+  package_name %w(golang
+                  libjpeg-turbo8-dev
+                  make
+                  tmux
+                  htop
+                  chromium-browser
+                  git
+                  cmake
+                  zlib1g-dev
+                  libjpeg-dev
+                  xvfb
+                  libav-tools
+                  xorg-dev
+                  python-opengl
+                  libboost-all-dev
+                  libsdl2-dev
+                  swig)
+end

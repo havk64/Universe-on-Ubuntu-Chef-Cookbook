d2837ab8ebe1eaeec8354b06cd202cabc3fe1a0e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-14 19:16:00 -0200

    Universe-ubuntu cookbook generated

---
+++ cookbooks/universe-ubuntu/README.md
@@ -0,0 +1,4 @@
+# universe-ubuntu
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/universe-ubuntu/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: universe-ubuntu
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.

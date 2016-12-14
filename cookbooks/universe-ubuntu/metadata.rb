2626c1f813b852873ad92764bda468e34ce779e0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-14 19:32:11 -0200

    Customize metadata file

--- cookbooks/universe-ubuntu/metadata.rb
+++ cookbooks/universe-ubuntu/metadata.rb
@@ -1,9 +1,9 @@
-name 'universe-ubuntu'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+name 'Universe on Ubuntu'
+maintainer 'Alexandro de Oliveira'
+maintainer_email 'alexandro.oliveira@holbertonschool.com'
 license 'all_rights'
-description 'Installs/Configures universe-ubuntu'
-long_description 'Installs/Configures universe-ubuntu'
+description 'Installs/Configures Universe on an Ubuntu Vagrant instance'
+long_description 'Installs/Configures Universe on an Ubuntu Vagrant instance'
 version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
@@ -13,3 +13,5 @@ version '0.1.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/universe-ubuntu' if respond_to?(:source_url)
+
+depends 'apt', '~> 4.0'

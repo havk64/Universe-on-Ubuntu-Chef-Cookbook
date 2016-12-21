c9c1b8c71d05808ccb44af3be3076a28b5e4b738
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-21 21:45:51 -0200

    Rename cookbook recipe, replace dash with underscore

--- cookbooks/universe_ubuntu/spec/unit/recipes/default_spec.rb
+++ cookbooks/universe_ubuntu/spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook Name:: universe-ubuntu
+# Cookbook Name:: universe_ubuntu
 # Spec:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'universe-ubuntu::default' do
+describe 'universe_ubuntu::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new

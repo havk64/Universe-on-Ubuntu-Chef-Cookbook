1e1e2624e5688af5d208f4ae56ac83cd20c62406
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-25 22:17:55 -0200

    Fix unit test converge error

--- cookbooks/universe_ubuntu/spec/unit/recipes/default_spec.rb
+++ cookbooks/universe_ubuntu/spec/unit/recipes/default_spec.rb
@@ -7,10 +7,15 @@
 require 'spec_helper'
 
 describe 'universe_ubuntu::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on an Ubuntu' do
+    before do
+      stub_command("[ -x /home/vagrant/anaconda3/bin/conda ]").and_return(0)
+    end
+
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
+      ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |node|
+        node.override['etc']['passwd']['vagrant']['dir'] = '/home/vagrant'
+      end.converge(described_recipe)
     end
 
     it 'converges successfully' do

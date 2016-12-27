0e71d00b575d51e8687a6791fd59bdd5e06c94df
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-27 19:38:06 -0200

    Add gpu flag to attributes file

--- cookbooks/universe_ubuntu/attributes/default.rb
+++ cookbooks/universe_ubuntu/attributes/default.rb
@@ -1,4 +1,5 @@
 default['universe']['user'] = 'vagrant'
+default['universe']['gpu'] =  false # Change to 'true' to enable gpu processing
 user = default['universe']['user']
 
 default['universe']['home'] = automatic['etc']['passwd'][user]['dir']

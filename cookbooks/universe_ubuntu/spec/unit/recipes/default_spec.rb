bee2771698609fc0a228a4942bf2861e3c50a01a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-27 19:39:48 -0200

    Add stub commands to test conda environment and tensorflow install

--- cookbooks/universe_ubuntu/spec/unit/recipes/default_spec.rb
+++ cookbooks/universe_ubuntu/spec/unit/recipes/default_spec.rb
@@ -10,11 +10,14 @@ describe 'universe_ubuntu::default' do
   context 'When all attributes are default, on an Ubuntu' do
     before do
       stub_command('[ -x /home/vagrant/anaconda3/bin/conda ]').and_return(0)
+      stub_command('[ -e /home/vagrant/anaconda3/envs/universe ]').and_return(0)
+      stub_command('[ -x /home/vagrant/anaconda3/envs/universe/bin/tensorboard ]').and_return(0)
     end
 
     let(:chef_run) do
       ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |node|
-        node.override['etc']['passwd']['vagrant']['dir'] = '/home/vagrant'
+        node.override['universe']['user'] = 'vagrant'
+        node.override['universe']['home'] = '/home/vagrant'
       end.converge(described_recipe)
     end
 

cee2905b3fbb3cc5895ba03466cce6c99f87be00
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-01-16 23:41:29 -0200

    Fix recipe name

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe universe-ubuntu::default
+# Inspec test for recipe universe_ubuntu::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html

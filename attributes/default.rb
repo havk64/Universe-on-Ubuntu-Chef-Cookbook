cd3dca0f8e371f795c37fe09c7fee51833fa709a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-01-20 17:29:06 -0200

    Cookstyle, prefer single-quoted strings

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['universe']['user']['name'] = 'vagrant'
 default['universe']['user']['home'] = '/home/vagrant'
 default['universe']['conda_env'][:CONDA_DEFAULT_ENV] = 'universe'
-default['universe']['conda_env'][:CONDA_PREFIX] = "/home/vagrant/anaconda3/envs/universe"
+default['universe']['conda_env'][:CONDA_PREFIX] = '/home/vagrant/anaconda3/envs/universe'
 default['universe']['conda_env'][:PATH] = "/home/vagrant/anaconda3/envs/universe/bin:#{ENV['PATH']}"
 default['universe']['gpu'] =  false # Change to 'true' to enable gpu processing
 gpu = node['universe']['gpu'] ? 'gpu' : 'cpu'

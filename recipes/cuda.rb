cc789071cf90c78978c9f50e70cca7c65bf09405
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-01-30 22:57:20 -0200

    Transfer cuda tasks to cuda recipe

--- recipes/cuda.rb
+++ recipes/cuda.rb
@@ -3,3 +3,25 @@
 # Recipe:: cuda
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{node['universe']['cuda']['debfile']}" do
+  source node['universe']['cuda']['source']
+  checksum node['universe']['cuda']['checksum']
+end
+
+dpkg_package "#{node['universe']['cuda']['debfile']}" do
+  source "#{Chef::Config[:file_cache_path]}/#{node['universe']['cuda']['debfile']}"
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+package 'cuda'
+
+ruby_block 'Add Cuda env variables' do
+  block do
+    file = Chef::Util::FileEdit.new "#{home}/.bashrc"
+    file.insert_line_if_no_match(/^export LD_LIBRARY_PATH/,
+    'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"')
+    file.insert_line_if_no_match(/^export CUDA_HOME/, 'export CUDA_HOME=/usr/local/cuda')
+    file.write_file
+  end
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -117,27 +117,7 @@ execute 'Install Tensorflow' do
   not_if "[ -x #{conda_prefix}/bin/tensorboard ]"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/#{node['universe']['cuda']['debfile']}" do
-  source node['universe']['cuda']['source']
-  checksum node['universe']['cuda']['checksum']
-end
-
-dpkg_package "#{node['universe']['cuda']['debfile']}" do
-  source "#{Chef::Config[:file_cache_path]}/#{node['universe']['cuda']['debfile']}"
-  notifies :run, 'execute[apt-get update]', :immediately
-end
-
-package 'cuda'
-
-ruby_block 'Add Cuda env variables' do
-  block do
-    file = Chef::Util::FileEdit.new "#{home}/.bashrc"
-    file.insert_line_if_no_match(/^export LD_LIBRARY_PATH/,
-    'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"')
-    file.insert_line_if_no_match(/^export CUDA_HOME/, 'export CUDA_HOME=/usr/local/cuda')
-    file.write_file
-  end
-end
+include_recipe 'universe_ubuntu::cuda'
 
 apt_repository 'docker' do
   uri 'https://apt.dockerproject.org/repo'

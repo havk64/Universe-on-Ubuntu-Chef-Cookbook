5a061a37e8aba7c3a43f49c5296a80f842ee4138
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-01 14:54:14 -0200

    Transfer unit test to openai spec file

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,10 +16,6 @@ describe 'universe_ubuntu::default' do
 
     let(:chef_run) do
       ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |node|
-        node.override['universe']['user']['name'] = 'vagrant'
-        node.override['universe']['user']['home'] = '/home/vagrant'
-        node.override['universe']['conda_env']['CONDA_PREFIX'] = '/home/vagrant/anaconda3/envs/universe'
-        node.override['universe']['conda_env']['PATH'] = "/home/vagrant/anaconda3/envs/universe/bin:#{ENV['PATH']}"
         node.override['universe']['gpu'] = true
       end.converge(described_recipe)
     end
@@ -44,6 +40,10 @@ describe 'universe_ubuntu::default' do
       expect(chef_run).to include_recipe('universe_ubuntu::docker')
     end
 
+    it 'Include openai recipe' do
+      expect(chef_run).to include_recipe('universe_ubuntu::openai')
+    end
+
     it 'customize unity launcher favorite apps' do
       expect(chef_run).to run_execute(
         'dbus-launch gsettings set com.canonical.Unity.Launcher favorites '\
@@ -59,44 +59,6 @@ describe 'universe_ubuntu::default' do
       expect(chef_run).to run_execute(
         'dbus-launch gsettings set org.gnome.desktop.default-applications.terminal '\
         "exec '/usr/bin/tilda'")
-    end
-
-    it 'Clone gym repo' do
-      expect(chef_run).to sync_git('/home/vagrant/openai/gym').with(user: 'vagrant')
-    end
-
-    it 'Clone universe repo' do
-      expect(chef_run).to sync_git('/home/vagrant/openai/universe').with(user: 'vagrant')
-    end
-
-    it 'Clone starter agent repo' do
-      expect(chef_run).to sync_git('/home/vagrant/openai/universe-starter-agent').with(user: 'vagrant')
-    end
-
-    it 'Install Gym modules' do
-      conda_prefix = '/home/vagrant/anaconda3/envs/universe'
-      expect(chef_run).to run_execute("#{conda_prefix}/bin/pip install -e '.[all]'")
-        .with(
-          user: 'vagrant',
-          cwd: '/home/vagrant/openai/gym',
-          environment: {
-            'CONDA_DEFAULT_ENV' => 'universe',
-            'CONDA_PREFIX' => conda_prefix,
-            'PATH' => "#{conda_prefix}/bin:#{ENV['PATH']}"
-          })
-    end
-
-    it 'Install Universe modules' do
-      conda_prefix = '/home/vagrant/anaconda3/envs/universe'
-      expect(chef_run).to run_execute("#{conda_prefix}/bin/pip install -e .")
-        .with(
-          user: 'vagrant',
-          cwd: '/home/vagrant/openai/universe',
-          environment: {
-            'CONDA_DEFAULT_ENV' => 'universe',
-            'CONDA_PREFIX' => conda_prefix,
-            'PATH' => "#{conda_prefix}/bin:#{ENV['PATH']}"
-          })
     end
   end
 end
--- spec/unit/recipes/openai_spec.rb
+++ spec/unit/recipes/openai_spec.rb
@@ -7,14 +7,52 @@
 require 'spec_helper'
 
 describe 'universe_ubuntu::openai' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on Ubuntu 14.04 platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'Clone gym repo' do
+      expect(chef_run).to sync_git('/home/vagrant/openai/gym').with(user: 'vagrant')
+    end
+
+    it 'Clone universe repo' do
+      expect(chef_run).to sync_git('/home/vagrant/openai/universe').with(user: 'vagrant')
+    end
+
+    it 'Clone starter agent repo' do
+      expect(chef_run).to sync_git('/home/vagrant/openai/universe-starter-agent').with(user: 'vagrant')
+    end
+
+    it 'Install Gym modules' do
+      conda_prefix = '/home/vagrant/anaconda3/envs/universe'
+      expect(chef_run).to run_execute("#{conda_prefix}/bin/pip install -e '.[all]'")
+        .with(
+          user: 'vagrant',
+          cwd: '/home/vagrant/openai/gym',
+          environment: {
+            'CONDA_DEFAULT_ENV' => 'universe',
+            'CONDA_PREFIX' => conda_prefix,
+            'PATH' => "#{conda_prefix}/bin:#{ENV['PATH']}"
+          })
+    end
+
+    it 'Install Universe modules' do
+      conda_prefix = '/home/vagrant/anaconda3/envs/universe'
+      expect(chef_run).to run_execute("#{conda_prefix}/bin/pip install -e .")
+        .with(
+          user: 'vagrant',
+          cwd: '/home/vagrant/openai/universe',
+          environment: {
+            'CONDA_DEFAULT_ENV' => 'universe',
+            'CONDA_PREFIX' => conda_prefix,
+            'PATH' => "#{conda_prefix}/bin:#{ENV['PATH']}"
+          })
     end
   end
 end

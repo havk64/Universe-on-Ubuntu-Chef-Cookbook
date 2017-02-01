44aced4f7a2e9fe5b2256da97ac44c82cad663e4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-01 14:55:06 -0200

    Transfer openai tasks to openai recipe file

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,11 +8,7 @@ include_recipe 'universe_ubuntu::essentials'
 include_recipe 'universe_ubuntu::tensorflow'
 include_recipe 'universe_ubuntu::cuda' if node['universe']['gpu']
 include_recipe 'universe_ubuntu::docker'
-
-user = node['universe']['user']['name']
-home = node['universe']['user']['home']
-conda_env = node['universe']['conda_env']
-conda_prefix = conda_env['CONDA_PREFIX']
+include_recipe 'universe_ubuntu::openai'
 
 execute 'Customize the Unity Launcher favorite apps' do
   command 'dbus-launch gsettings set com.canonical.Unity.Launcher favorites '\
@@ -27,39 +23,4 @@ end
 execute 'Set default terminal emulator' do
   command 'dbus-launch gsettings set org.gnome.desktop.default-applications.terminal '\
     "exec '/usr/bin/tilda'"
-end
-
-git "#{home}/openai/gym" do
-  user user
-  repository 'https://github.com/openai/gym.git'
-  revision 'master'
-  action :sync
-end
-
-git "#{home}/openai/universe" do
-  user user
-  repository 'https://github.com/openai/universe.git'
-  revision 'master'
-  action :sync
-end
-
-git "#{home}/openai/universe-starter-agent" do
-  user user
-  repository 'https://github.com/openai/universe-starter-agent.git'
-  revision 'master'
-  action :sync
-end
-
-execute 'Install gym modules' do
-  user user
-  environment conda_env
-  cwd "#{home}/openai/gym"
-  command "#{conda_prefix}/bin/pip install -e '.[all]'"
-end
-
-execute 'Install Universe modules' do
-  user user
-  environment conda_env
-  cwd "#{home}/openai/universe"
-  command "#{conda_prefix}/bin/pip install -e ."
 end
--- recipes/openai.rb
+++ recipes/openai.rb
@@ -3,3 +3,43 @@
 # Recipe:: openai
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+user = node['universe']['user']['name']
+home = node['universe']['user']['home']
+conda_env = node['universe']['conda_env']
+conda_prefix = conda_env['CONDA_PREFIX']
+
+git "#{home}/openai/gym" do
+  user user
+  repository 'https://github.com/openai/gym.git'
+  revision 'master'
+  action :sync
+end
+
+git "#{home}/openai/universe" do
+  user user
+  repository 'https://github.com/openai/universe.git'
+  revision 'master'
+  action :sync
+end
+
+git "#{home}/openai/universe-starter-agent" do
+  user user
+  repository 'https://github.com/openai/universe-starter-agent.git'
+  revision 'master'
+  action :sync
+end
+
+execute 'Install gym modules' do
+  user user
+  environment conda_env
+  cwd "#{home}/openai/gym"
+  command "#{conda_prefix}/bin/pip install -e '.[all]'"
+end
+
+execute 'Install Universe modules' do
+  user user
+  environment conda_env
+  cwd "#{home}/openai/universe"
+  command "#{conda_prefix}/bin/pip install -e ."
+end

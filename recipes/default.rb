796362553b1a25ef7651af8b03a36d2eeffed12f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-02 01:36:09 -0200

    Task to create test template

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,3 +9,9 @@ include_recipe 'universe_ubuntu::tensorflow'
 include_recipe 'universe_ubuntu::cuda' if node['universe']['gpu']
 include_recipe 'universe_ubuntu::docker'
 include_recipe 'universe_ubuntu::openai'
+
+template "#{node['universe']['user']['home']}/universe_test.py" do
+  owner node['universe']['user']['name']
+  group node['universe']['user']['name']
+  source 'universe_test.erb'
+end
